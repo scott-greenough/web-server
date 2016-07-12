037ad40c75b30dcfbf1c9a0ef756da1f44fe02dc
Scott Greenough <scott.greenough@gmail.com>
2016-07-12 12:32:34 -0400

    initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next){
+		console.log('private route hit!');
+		next()
+	},
+	logger: function (req, res, next) {
+		console.log(new Date().toString() + ' -> Request: ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+	res.send('About us');
+})
+
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname);
+	
+app.listen(port, function() {
+	console.log('Express server started on port ' + port);
+});
